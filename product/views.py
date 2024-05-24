ced419cec2dda67e5b8c5f33f8b9fee6200cc2f7
Fer_zee <fersyvanderzee@gmail.com>
2024-05-24 14:57:20 +0200

    Updated views

--- product/views.py
+++ product/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Product
+
+def index(request):
+    product_list = Product.objects.all()
+    template = loader.get_template("product/products.html")
+    context = {
+        "product_list" : product_list,
+    }
+
+    return HttpResponse(template.render(context, request))
